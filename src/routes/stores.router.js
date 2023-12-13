21d33996b4de6f0bc0420a030da64c82843bde48
jeongseon0 <86090167+jeongseon0@users.noreply.github.com>
2023-12-13 17:02:06 +0900

    🩹 fix | 경로 수정

--- src/routes/stores.router.js
+++ src/routes/stores.router.js
@@ -4,9 +4,9 @@ import { StoresController } from '../modules/stores/stores.controller.js';
 const storesRouter = Router();
 const storesController = new StoresController();
 
-storesRouter.post('/open', storesController.open);
-storesRouter.get('/:user_id/enter', storesController.enter);
-storesRouter.patch('/interior', storesController.interior);
-storesRouter.post('/close', storesController.close);
+storesRouter.post('', storesController.open);
+storesRouter.get('/:user_id', storesController.enter);
+storesRouter.patch('/:user_id', storesController.remodelling);
+storesRouter.post('/:user_id', storesController.close);
 
 export default storesRouter;
