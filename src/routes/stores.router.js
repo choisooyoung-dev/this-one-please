6400e039efb9de09d0ccacbbca7b9032b3b86d0d
jeongseon0 <86090167+jeongseon0@users.noreply.github.com>
2023-12-13 17:06:32 +0900

    🩹 fix | store api 경로 수정

--- src/routes/stores.router.js
+++ src/routes/stores.router.js
@@ -5,8 +5,8 @@ const storesRouter = Router();
 const storesController = new StoresController();
 
 storesRouter.post('', storesController.open);
-storesRouter.get('/:user_id', storesController.enter);
-storesRouter.patch('/:user_id', storesController.remodelling);
-storesRouter.post('/:user_id', storesController.close);
+storesRouter.get('/:id', storesController.enter);
+storesRouter.patch('/:id', storesController.remodelling);
+storesRouter.post('/:id', storesController.close);
 
 export default storesRouter;
