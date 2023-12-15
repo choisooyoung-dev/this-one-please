cae9dc908d6119b71fc447c9529f3173845ede84
da <dain940209@gmail.com>
2023-12-15 20:06:00 +0900

    text: response 구간 리턴

--- src/modules/carts/carts.controller.js
+++ src/modules/carts/carts.controller.js
@@ -15,7 +15,7 @@ export class CartsController {
             );
             console.log('controller: ', createdCart);
 
-            res.status(201).json({ data: createdCart, message: '성공' });
+            return res.status(201).json({ data: createdCart, message: '성공' });
         } catch (e) {
             console.log(e);
             res.status(500).json({ message: '예상치못한에러입니다' });
@@ -25,13 +25,9 @@ export class CartsController {
     getCarts = async (req, res, next) => {
         try {
             const user_id = res.locals.user.id;
-            // const cart = await this.cartsService.getCart(id);
-            // if (cart.user_id !== res.locals.user.id) {
-            //     res.status(400).json({ message: '권한이 없습니다' });
-            // }
             const carts = await this.cartsService.getCarts(user_id);
             console.log(carts);
-            res.status(201).json({ data: carts, message: '성공' });
+            return res.status(201).json({ data: carts, message: '성공' });
         } catch (e) {
             console.log(e);
             res.status(500).json({ message: '예상치못한에러입니다' });
@@ -43,12 +39,13 @@ export class CartsController {
             // const user_id = res.locals.user.id;
             const id = Number(req.params.id);
             const { count } = req.body;
+            
             const cart = await this.cartsService.getCart(id);
             if (cart.user_id !== res.locals.user.id) {
-                res.status(400).json({ message: '권한이 없습니다' });
+                return res.status(400).json({ message: '권한이 없습니다' });
             }
             const updatedCart = await this.cartsService.updateCart(id, count);
-            res.status(201).json({
+            return res.status(201).json({
                 data: updatedCart,
                 message: '업데이트가 되었습니다',
             });
@@ -63,14 +60,14 @@ export class CartsController {
             const user_id = res.locals.user.id;
             const cart = await this.cartsService.getCart(id);
             if (cart.user_id !== res.locals.user.id) {
-                res.status(400).json({ message: '권한이 없습니다' });
+                return res.status(400).json({ message: '권한이 없습니다' });
             }
             const { id } = req.params;
             if (!cart) {
-                res.status(404).json({ message: '삭제할 주문목록이 없습니다' });
+                return res.status(404).json({ message: '삭제할 주문목록이 없습니다' });
             }
             await this.cartsService.deleteCart(id);
-            res.status(201).json({ message: '삭제 성공' });
+            return res.status(201).json({ message: '삭제 성공' });
         } catch (e) {
             console.log(e);
             res.status(500).json({ message: '예상치 못한 에러입니다' });
