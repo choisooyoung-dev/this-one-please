cad56689a872460e48fc8a1d788d01cc133de4d1
da <dain940209@gmail.com>
2023-12-18 08:42:02 +0900

    test : redis

--- auth-utils/redis.util.js
+++ auth-utils/redis.util.js
@@ -2,9 +2,13 @@ import redis from 'redis';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const redisPort = process.env.REDIS_PORT;
+const redisHost = process.env.REDIS_HOST; // 변경된 부분
+
+
 const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST, // 여기에 원하는 호스트를 설정하세요
-  port: process.env.REDIS_PORT,
+  host: redisHost, // 여기에 원하는 호스트를 설정하세요
+  port: redisPort,
 });
 redisClient.on('connect', () => console.log('Connected to Redis!'));
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
