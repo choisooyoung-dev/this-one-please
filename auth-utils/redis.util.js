7cf429044f368887a298f6de756c28c66f012b1b
da <dain940209@gmail.com>
2023-12-18 08:09:27 +0900

    test : redis

--- auth-utils/redis.util.js
+++ auth-utils/redis.util.js
@@ -1,7 +1,7 @@
 import redis from 'redis';
 
 const redisClient = redis.createClient({
-  host: process.env.REDIS_PORT, // 여기에 원하는 호스트를 설정하세요
+  host: process.env.REDIS_HOST, // 여기에 원하는 호스트를 설정하세요
   port: process.env.REDIS_PORT,
 });
 redisClient.on('connect', () => console.log('Connected to Redis!'));
